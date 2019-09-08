985a5246e4b03b662c0c43099993f0d559070f42
Akshay Shinde <akshayshinde281195@gmail.com>
2019-09-08 13:21:08 +0530

    Create MQ2.py

---
+++ MQ2.py
@@ -0,0 +1,22 @@
+#download Library from Below path
+#https://github.com/tutRPi/Raspberry-Pi-Gas-Sensor-MQ
+#then Execute this code
+
+
+from mq import *
+import sys, time
+
+try:
+    print("Press CTRL+C to abort.")
+    
+    mq = MQ();
+    while True:
+        perc = mq.MQPercentage()
+        sys.stdout.write("\r")
+        sys.stdout.write("\033[K")
+        sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+except:
+    print("\nAbort by user")
