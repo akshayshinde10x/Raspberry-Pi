02ca87d9f217b12719a01ac3c657fea6bdb111db
Akshay Shinde <akshayshinde281195@gmail.com>
2021-11-16 09:57:17 +0530

    Update LEDwithSlider.py

--- adafruit/LEDwithSlider.py
+++ adafruit/LEDwithSlider.py
@@ -5,11 +5,8 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)  
 pwm = GPIO.PWM(12, 100) 
 
-
-
 ADAFRUIT_IO_USERNAME = "abc"
 ADAFRUIT_IO_KEY = "aio_uzTw50EeIqMExkImW61EVL9H8PUX"
-
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
@@ -18,11 +15,8 @@ status=0
 pwm.start(status)                   
 
 while True:
-	
 	status = aio.receive(slider_feed.key).value
-	
 	pwm.ChangeDutyCycle(int(status))
 	time.sleep(0.001)
 pwm.stop()                        
 GPIO.cleanup()
-
