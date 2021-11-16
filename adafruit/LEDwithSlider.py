fb9c77fc82676e3fa9dc1d80a1525ebb44dd76cf
Akshay Shinde <akshayshinde281195@gmail.com>
2021-11-16 09:55:44 +0530

    Create LEDwithSlider.py

---
+++ adafruit/LEDwithSlider.py
@@ -0,0 +1,28 @@
+from Adafruit_IO import Client
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)  
+pwm = GPIO.PWM(12, 100) 
+
+
+
+ADAFRUIT_IO_USERNAME = "abc"
+ADAFRUIT_IO_KEY = "aio_uzTw50EeIqMExkImW61EVL9H8PUX"
+
+
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+slider_feed = aio.feeds('slider')
+status=0                               
+pwm.start(status)                   
+
+while True:
+	
+	status = aio.receive(slider_feed.key).value
+	
+	pwm.ChangeDutyCycle(int(status))
+	time.sleep(0.001)
+pwm.stop()                        
+GPIO.cleanup()
+
